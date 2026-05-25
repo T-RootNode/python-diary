3119a212c428e86f44175059c66a52442b2e1081
AusArtn <thorsten.tummer@gmail.com>
2026-05-25 20:49:32 +0200

    Spelling errors corrected; program can now be easily terminated.

--- 06-guess-game.py
+++ 06-guess-game.py
@@ -2,24 +2,33 @@ import random
 
 rand_number = random.randint(1, 20)
 versuche = 5
-print("gesucht ist eine zahl zwischen 1 und 20, du hast 5 versuche findest du sie?")
+
+print("Gesucht ist eine Zahl zwischen 1 und 20. Du hast 5 Versuche. Findest du sie?")
+
 while True:
-    guess_raw = input("bitte gib eine tahl ein du hast:")
+    guess_raw = input("Bitte gib eine Zahl ein: ")
+
     if not guess_raw.isdigit():
-        print(f"leider war das keine zahl.")
+        print("Leider war das keine Zahl.")
         continue
 
     guess = int(guess_raw)
 
     if guess == rand_number:
         print("Gewonnen!")
+        print("Drücke ENTER zum Beenden...")
+        input()
         break
     elif guess > rand_number:
         versuche -= 1
-        print(f"leider zu hoch du hast noch {versuche} versuche.")
+        print(f"Leider zu hoch! Du hast noch {versuche} Versuche.")
     elif guess < rand_number:
         versuche -= 1
-        print(f"leider zu niedrieg du hast noch {versuche} versuche.")
+        print(f"Leider zu niedrig! Du hast noch {versuche} Versuche.")
+
     if versuche == 0:
-        print("leider verloren")
+        print("Leider verloren!")
+        print(f"Die Zahl war {rand_number}")
+        print("Drücke ENTER zum Beenden...")
+        input()
         break
